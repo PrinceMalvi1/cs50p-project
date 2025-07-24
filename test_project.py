272c1e5bdde7617b47cd6f62d1e2f3c2aaba193d
Prince Malvi <malvip730@gmail.com>
2025-07-24 15:08:14 +0000

    Update

--- test_project.py
+++ test_project.py
@@ -12,3 +12,12 @@ def test_get_summary_single():
     ]
     result = get_summary(expenses)
     assert result == {"Transport": 30.0}
+
+def test_get_summary_multiple():
+    expenses = [
+        {"amount": 20.0, "category": "Food", "date": "2025-07-22"},
+        {"amount": 30.0, "category": "Rent", "date": "2025-07-22"},
+        {"amount": 10.0, "category": "Food", "date": "2025-07-23"},
+    ]
+    result = get_summary(expenses)
+    assert result == {"Food": 30.0, "Rent": 30.0}
